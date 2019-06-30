479216e5f201b22336054ebec1d1752228f1b577
bikal3 <email_id>
2019-06-30 18:14:25 +0545

    added feature through api

--- app/src/main/java/com/example/travelnepalapp/API/FeedbackAPI.java
+++ app/src/main/java/com/example/travelnepalapp/API/FeedbackAPI.java
@@ -1,4 +1,22 @@
 package com.example.travelnepalapp.API;
 
-public class FeedbackAPI {
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface FeedbackAPI {
+
+
+
+    @FormUrlEncoded
+    @POST("contact")
+    Call<String> addpost(@Field("fname") String fname,
+                         @Field("lname")String lname,
+                         @Field("email") String email,
+                         @Field("message") String message
+    );
 }
+
+
+
--- app/src/main/java/com/example/travelnepalapp/Feedback/Feedback.java
+++ app/src/main/java/com/example/travelnepalapp/Feedback/Feedback.java
@@ -2,14 +2,73 @@ package com.example.travelnepalapp.Feedback;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.travelnepalapp.API.FeedbackAPI;
 import com.example.travelnepalapp.R;
+import com.example.travelnepalapp.Retrofit.RetrofitHelper;
 
-public class Feedback extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Feedback extends AppCompatActivity implements View.OnClickListener {
+
+
+    EditText fname, lname, email, message;
+    Button btnfeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        fname = findViewById(R.id.et_feedback_fname);
+        lname = findViewById(R.id.et_feedback_lname);
+        email = findViewById(R.id.et_feedback_email);
+        message = findViewById(R.id.et_feedback_feecback);
+
+        btnfeedback = findViewById(R.id.btn_feedback_add);
+
+        btnfeedback.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_feedback_add:
+                addfeedback();
+                break;
+        }
+    }
+
+    private void addfeedback() {
+        String ffname=fname.getText().toString();
+        String llname=lname.getText().toString();
+        String emails=email.getText().toString();
+        String messages=message.getText().toString();
+
+
+        FeedbackAPI feedbackAPI= RetrofitHelper.instance().create(FeedbackAPI.class);
+        Call<String> feedbackcall=feedbackAPI.addpost(ffname,llname,emails,messages);
+        feedbackcall.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Toast.makeText(Feedback.this, "Successfully Added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(Feedback.this, "Error"+t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+
 }
